3b7a77b4ad0dc06b9fcc36c66715364a16a1c0dd
Asha Baker <asha.baker@hotmail.com>
2018-03-19 12:14:41 -0400

    final commit

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "",
+  user: "",
+  password: "",
+  database: ""
+});
+onnection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
